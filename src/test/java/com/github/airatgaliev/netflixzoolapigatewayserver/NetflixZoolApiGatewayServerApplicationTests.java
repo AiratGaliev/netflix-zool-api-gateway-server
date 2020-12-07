8b47769132af744bb1c595f97c2482035a3e91fc
Airat Galiev <airat.galiev@gmail.com>
2020-12-08 00:36:53 +0300

    created new project

---
+++ src/test/java/com/github/airatgaliev/netflixzoolapigatewayserver/NetflixZoolApiGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.airatgaliev.netflixzoolapigatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixZoolApiGatewayServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
